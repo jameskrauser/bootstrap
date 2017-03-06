ad45fabf0f23528b3328c1eee0ba0f9544cb92d2
jameskrauser <jameskrauser@gmail.com>
2017-03-06 15:40:00 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    
-/* 	$('#contact form').on('submit', function(e) {
+	//not work
+  	$('#contact form').on('submit', function(e) {	 
         e.preventDefault();
         var $form = $(e.currentTarget),
             $email = $form.find('#contact-email'),
@@ -13,7 +13,30 @@ $(document).ready(function() {
             $button.attr('disabled', 'disabled');
             $button.after('<span>訊息已送出。我們將盡快與你聯繫</span>');
         }
-    }); */
+    });
+	
+	 $('#real_login').on('click',function(e) {
+		e.preventDefault();
+        var $account= $("#signin-account").val();
+		var $pwd=$("#signin-password").val();
+		
+		if( $account=='mitac' && $pwd=='mitac' ){
+			
+			//$button.after('<span>登入成功...</span>');
+			//alert("account ok");
+			//window.location='contact.html';
+			document.getElementById("login_name").innerHTML =  "<span style='color:#FF0000'> 登入成功 </span>";
+			
+		}
+		else{
+			
+			//$button.after('<span>帳號或密碼錯誤</span>');
+			//alert("account fail");
+			document.getElementById("login_name").innerHTML = '登入失敗!';
+		}
+		 
+		  
+	 });
 	
 	
 	//按下登入,彈出帳號及密碼輸入欄
@@ -22,32 +45,6 @@ $(document).ready(function() {
         $('form#signin').fadeIn('fast');
     });
 	
-	
-	////按下登入,彈出帳號及密碼輸入欄
-    $('#sign-btn2').on('click', function(e) {
-        $(e.currentTarget).closest('ul').hide();
-        $('form#signin').fadeIn('fast');
-    });
-	
-	
- 
-function validate(a1,a2)
-{ 
-  if (a1=='101' && a2=='001') 
-      load('http://www.google.com.tw');  //輸入帳號101、密碼001時會進入google
-
-  else if  (a1=='102' && a2=='002') 
-      load('http://www.pchome.com.tw/');  //輸入帳號102、密碼002時會進入pchome
-
-  else
-      load('http://tw.yahoo.com'); //輸入錯的密碼時會進入奇摩網頁
-
-}
-
-function load(url)
-{
-location.href=url;
-}
- 
+	 
 
 });
