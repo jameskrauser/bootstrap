274dd0635054066b2a1e047ce90fea5c9e6c9e14
jameskrauser <jameskrauser@gmail.com>
2017-05-16 10:45:23 +0800

    remove some unuse code in main.js at real_login

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
 $(document).ready(function() {
-	//not work
-  	$('#contact form').on('submit', function(e) {	 
-        e.preventDefault();
-        var $form = $(e.currentTarget),
-            $email = $form.find('#contact-email'),
-            $button = $form.find('button[type=submit]');
-        if($email.val().indexOf('@') == -1) {
-            vaca = $email.closest('form-group')
-            $email.closest('.form-group').addClass('has-error');
-        } else {
-            $form.find('.form-group').addClass('has-success').removeClass('has-error');
-            $button.attr('disabled', 'disabled');
-            $button.after('<span>訊息已送出。我們將盡快與你聯繫</span>');
-        }
-    });
 	
 	 $('#real_login').on('click',function(e) {
 		e.preventDefault();
@@ -21,30 +6,19 @@ $(document).ready(function() {
 		var $pwd=$("#signin-password").val();
 		
 		if( $account=='mitac' && $pwd=='mitac' ){
-			
-			//$button.after('<span>登入成功...</span>');
-			//alert("account ok");
-			// $('form#signin').fadeIn('fast');
-			
-			//javascript style
-			//var a = document.getElementById("signin-account");		
-			//a.style.display = 'none';
-			
-			
+			//把帳號,密碼TEXT及登入按鈕做隱藏
 			$("#signin-account").fadeTo("slow",0);
 			$("#signin-password").fadeTo("slow",0);
 			$("#real_login").fadeTo("slow",0);
-			document.getElementById("login_name").innerHTML =  "<span style='color:#FF0000; font-size:8em'> 登入成功 </span>";
+			//回傳訊息
+			document.getElementById("login_name").innerHTML =  
+			"<span style='color:#FF0000; font-size:8em'> 登入成功 </span>";
 			
 		}
 		else{
-			
-			//$button.after('<span>帳號或密碼錯誤</span>');
-			//alert("account fail");
-			document.getElementById("login_name").innerHTML = "<span style='color:#FF0000; font-size:8em'>登入失敗  </span>";
+			document.getElementById("login_name").innerHTML = 
+			"<span style='color:#FF0000; font-size:8em'>登入失敗  </span>";
 		}
-		 
-		  
 	 });
 	
 	
